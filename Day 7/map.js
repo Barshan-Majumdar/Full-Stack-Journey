87404a198ac1c4f92ca728aa796e6b8640179fa3
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-09 20:51:42 +0530

    Day 7

--- Day 7/map.js
+++ Day 7/map.js
@@ -1,3 +1,12 @@
 const a = [1, 2, 3 , 4, 5];
 const b = a.map(x => x * 2);
 console.log(b); // [2, 4, 6, 8, 10]
+
+const users = [{
+    fName : 'Barshan',
+    lNAme : 'Majumdar',
+    age : '20'
+
+}];
+
+console.log(users.map(user => user.fName)); // ['Barshan']
