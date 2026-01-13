e2239f23b44c4f5e5a62738fee80b89352bb7774
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-13 16:16:44 +0530

    Day 11

---
+++ Day 11/setTimer.js
@@ -0,0 +1,7 @@
+console.log("This is a asynchronous timer example.");
+
+setTimeout(() => {
+    console.log("This message is displayed after a 2 second delay.");
+}, 2000);
+
+console.log("This message is displayed immediately after setting the timer.");
