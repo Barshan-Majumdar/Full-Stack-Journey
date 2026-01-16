22d9a840578794549a7973ca8f33ca69a282a817
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-16 22:42:50 +0530

    Day 13

---
+++ Day 13/combining_promises.js
@@ -0,0 +1,9 @@
+const firstObject = fetch("https://dummyjson.com/users/1").then(res => res.json());
+const secondObject = fetch("https://dummyjson.com/posts/1").then(res => res.json());
+const thirdObject = fetch("https://dummyjson.com/products/1").then(res => res.json());
+
+Promise.all([firstObject, secondObject, thirdObject]).then(responese => {
+    for (const response of responese){
+        console.log(response);
+    }
+})
