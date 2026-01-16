d39339c13cd0c9d7a86abbb30570fd61e71ff984
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-16 23:02:06 +0530

    Day 13

---
+++ Day 13/async.js
@@ -0,0 +1,14 @@
+async function fetchData(){
+    const responce = await fetch("https://dummyjson.com/ip");
+    const data = await responce.json();
+    console.log(data);
+}
+
+function fetchData1(){
+    const responce = fetch("https://dummyjson.com/ip").then(res => res.json()).then(data => console.log(data));
+    responce.catch(err => console.log("Error:", err)); 
+}
+
+//both can be used for same funcionality
+fetchData();
+fetchData1();
