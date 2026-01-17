8f81688b1ab6c33b49b2ef429be0212624cbc675
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-17 11:32:12 +0530

    Day 14

---
+++ Day 14/LocalStorage/localStorage.js
@@ -0,0 +1,7 @@
+localStorage.setItem("name", "Barshan Majumdar");
+localStorage.setItem("age", "21");
+localStorage.setItem("goal", "Full Stack JavaScript");
+
+console.log(localStorage.length);
+console.log(localStorage.getItem("name"));
+console.log(localStorage.key(0));
