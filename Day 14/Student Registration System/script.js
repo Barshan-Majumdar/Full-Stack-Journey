dc54510119b1a4d927148f3e6fe800b03af7071d
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-17 22:08:16 +0530

    Day 14

--- Day 14/Student Registration System/script.js
+++ Day 14/Student Registration System/script.js
@@ -1,5 +1,5 @@
-const mainTag = document.querySelector("main");
-
+// const mainTag = document.querySelector("main");
+const tableBody = document.querySelector("tbody");
 const nameInput = document.querySelector(".studentName");
 const classInput = document.querySelector(".studentClass");
 const addressInput = document.querySelector(".addressArea");
@@ -11,21 +11,37 @@ form.addEventListener("submit", (event) => {
     console.log("Class : "+ classInput.value);
     console.log("Address : "+ addressInput.value);
     console.log("Contact Number : "+ contactInput.value);
-    const studentCard = document.createElement("div");
-    const sName = document.createElement("p");
-    sName.innerHTML = nameInput.value;
-    const sClass = document.createElement("p");
-    sClass.innerHTML = classInput.value;
-    const sAddress = document.createElement("p");
-    sAddress.innerHTML = addressInput.value;
-    const sContact = document.createElement("p");
-    sContact.innerHTML = contactInput.value;
-    studentCard.appendChild(sName);
-    studentCard.appendChild(sClass);
-    studentCard.appendChild(sAddress);
-    studentCard.appendChild(sContact);
+    // const studentCard = document.createElement("div");
+    // const sName = document.createElement("p");
+    // sName.innerHTML = nameInput.value;
+    // const sClass = document.createElement("p");
+    // sClass.innerHTML = classInput.value;
+    // const sAddress = document.createElement("p");
+    // sAddress.innerHTML = addressInput.value;
+    // const sContact = document.createElement("p");
+    // sContact.innerHTML = contactInput.value;
+    // studentCard.appendChild(sName);
+    // studentCard.appendChild(sClass);
+    // studentCard.appendChild(sAddress);
+    // studentCard.appendChild(sContact);
+    const row = document.createElement("tr");
+    const tName = document.createElement("td");
+    tName.textContent = nameInput.value;
+    const tClass = document.createElement("td");
+    tClass.textContent = classInput.value;
+    const tAddress = document.createElement("td");
+    tAddress.textContent = addressInput.value;
+    const tContact = document.createElement("td");
+    tContact.textContent = contactInput.value;
 
-    mainTag.appendChild(studentCard);
+    row.appendChild(tName);
+    row.appendChild(tClass);
+    row.appendChild(tAddress);
+    row.appendChild(tContact);
+
+    tableBody.appendChild(row);
+    
+    // mainTag.appendChild(studentCard);
     
 });
 
