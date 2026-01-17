0e1d7c4d1d95f21824b318a825435796ddda4f7f
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-17 11:20:10 +0530

    Day 14

--- Day 14/bom.js
+++ Day 14/bom.js
@@ -1,1 +1,11 @@
-alert("Hey there");
+alert("Enter you full name please !!");
+const name = prompt("Enter your name");
+alert("Welcome " + name + " to our website");
+
+ const isOk = confirm("Do you wanna add your name to console ??");
+
+ if (isOk) {
+    console.log("User's name is: " + name);
+ } else{
+    console.log("Permisson denied");    
+ }
