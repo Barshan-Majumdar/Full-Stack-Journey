68efaba76843572e37aaa20329198024d89c73ef
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-01-17 20:58:49 +0530

    Day 14

--- Day 14/Student Registration System/script.js
+++ Day 14/Student Registration System/script.js
@@ -1,12 +1,32 @@
+const mainTag = document.querySelector("main");
+
 const nameInput = document.querySelector(".studentName");
 const classInput = document.querySelector(".studentClass");
 const addressInput = document.querySelector(".addressArea");
 const contactInput = document.querySelector(".contactNo");
 const form = document.querySelector("form");
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
     console.log("Name : "+ nameInput.value);
     console.log("Class : "+ classInput.value);
     console.log("Address : "+ addressInput.value);
     console.log("Contact Number : "+ contactInput.value);
+    const studentCard = document.createElement("div");
+    const sName = document.createElement("p");
+    sName.innerHTML = nameInput.value;
+    const sClass = document.createElement("p");
+    sClass.innerHTML = classInput.value;
+    const sAddress = document.createElement("p");
+    sAddress.innerHTML = addressInput.value;
+    const sContact = document.createElement("p");
+    sContact.innerHTML = contactInput.value;
+    studentCard.appendChild(sName);
+    studentCard.appendChild(sClass);
+    studentCard.appendChild(sAddress);
+    studentCard.appendChild(sContact);
+
+    mainTag.appendChild(studentCard);
+    
 });
+
 
